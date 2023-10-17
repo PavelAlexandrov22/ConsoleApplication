af9dd7fa2d72f77ea6c2ee1d052d9e0c13fbddb6
Pasha <alexandrov-pasha@yandex.ru>
2023-10-17 12:45:41 +0300

    firstapp

--- src/main/java/org/example/Loader.java
+++ src/main/java/org/example/Loader.java
@@ -7,14 +7,17 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class Loader {
     public static void main(String[] args) throws IOException {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Введите фамилию: ");
             String lastName = reader.readLine();
+            System.out.print("Введите имя: ");
             String firstName = reader.readLine();
 
-            String urlParameters = "?firstName=" + URLEncoder.encode(firstName, "UTF-8") + "&lastName=" + URLEncoder.encode(lastName, "UTF-8");
+            String urlParameters = "?firstName=" + URLEncoder.encode(firstName, StandardCharsets.UTF_8) + "&lastName=" + URLEncoder.encode(lastName, "UTF-8");
             String url = "http://localhost:8080/Servlets/hello_with_name" + urlParameters;
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -22,11 +25,11 @@ public class Loader {
 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
-            System.out.println(response.toString());
+            System.out.println(response);
         }
     }
 
